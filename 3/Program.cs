314749c81afed4bc9373489f4dd71d060ef5726d
Bobbislavski <robertkurovsci23@gmail.com>
2023-10-31 10:52:15 +0300

    labab3c

---
+++ 3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SmallestDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное трехзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int smallestDigit = 9;
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit < smallestDigit)
+                {
+                    smallestDigit = digit;
+                }
+                number /= 10;
+            }
+
+            Console.WriteLine($"Наименьшая цифра в числе: {smallestDigit}");
+        }
+    }
+}
